71d4ab6c2123c4a7633b065635ae831f0933b656
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-07 22:33:04 -0600

    get users done

--- api/server.js
+++ api/server.js
@@ -5,5 +5,24 @@ const server = express();
 
 server.use(express.json());
 
+const User = require('./users/model');
+
+server.get('/api/users', (req, res) =>
+{
+
+    User.find()
+        .then(users =>
+        {
+            res.json(users);
+        })
+        .catch(err =>
+        {
+            res.status(500).json({
+                message: 'The users information could not be retrieved',
+                err: err.message,
+                stack: err.stack
+            });
+        });
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
