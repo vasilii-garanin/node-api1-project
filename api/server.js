432ffaa3e4ace9af0392f9b9e344c921a21a6b86
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-07 22:40:54 -0600

    api get by id working

--- api/server.js
+++ api/server.js
@@ -25,4 +25,27 @@ server.get('/api/users', (req, res) =>
         });
 });
 
+server.get('/api/users/:id', (req, res) =>
+{
+    User.findById(req.params.id)
+        .then(user =>
+        {
+            if (!user)
+            {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist'
+                });
+            }
+            res.json(user);
+        })
+        .catch(err =>
+        {
+            res.status(500).json({
+                message: 'The user information could not be retrieved',
+                err: err.message,
+                stack: err.stack
+            });
+        });
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
